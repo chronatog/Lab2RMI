b983bd7156c948575e9dd6ddfd51be456e6fe2cc
lil0 <elisabeth.bauernhofer@gmail.com>
2012-11-12 18:56:52 +0100

    BillingServer

--- Verteilte Systeme - Lab2 - RMI/src/billingServer/BillingServer.java
+++ Verteilte Systeme - Lab2 - RMI/src/billingServer/BillingServer.java
@@ -16,6 +16,44 @@ public class BillingServer {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+
     }
+
+
+
+    public PriceSteps getPriceSteps(){
+
+        return null;
+    }
+
+    public void createPriceStep(double startPrice, double endPrice, double fixedPrice, double variablePricePercent){
+
+    }
+
+    public void deletePriceStep(double startPrice, double endPrice){
+
+    }
+
+    public void billAuction(String user, long auctionID, double price){
+
+    }
+
+    public Bill getBill(String user){
+        return null;
+    }
+
+     private static class Bill {
+
+        public Bill() {
+        }
+    }
+
+    private static class PriceSteps {
+
+        public PriceSteps() {
+        }
+    }
+
 
 }
