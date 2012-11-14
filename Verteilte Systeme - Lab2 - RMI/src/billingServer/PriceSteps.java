a591a0bb2d25b98b4038ba64ad4502d31e83a773
lil0 <elisabeth.bauernhofer@gmail.com>
2012-11-14 16:11:18 +0100

    billingServer
    Struktur festgelegt

---
+++ Verteilte Systeme - Lab2 - RMI/src/billingServer/PriceSteps.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package billingServer;
+
+/**
+ *
+ * @author lisibauernhofer
+ */
+class PriceSteps {
+
+}
