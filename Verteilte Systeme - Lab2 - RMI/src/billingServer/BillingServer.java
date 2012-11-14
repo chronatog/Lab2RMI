27f64150aa1c874769ab6cbf593fa3b1918e8d94
lil0 <elisabeth.bauernhofer@gmail.com>
2012-11-14 16:15:53 +0100

    BillingServer
    Grundstruktur erstellt

--- Verteilte Systeme - Lab2 - RMI/src/billingServer/BillingServer.java
+++ Verteilte Systeme - Lab2 - RMI/src/billingServer/BillingServer.java
@@ -4,56 +4,17 @@
  */
 
 package billingServer;
+import java.rmi.*;
+import java.rmi.server.*;
 
 /**
  *
  * @author lisibauernhofer
  */
-public class BillingServer {
+public class BillingServer implements BillingServerInt{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-
-
-    }
-
-
-
-    public PriceSteps getPriceSteps(){
-
-        return null;
-    }
-
-    public void createPriceStep(double startPrice, double endPrice, double fixedPrice, double variablePricePercent){
-
-    }
-
-    public void deletePriceStep(double startPrice, double endPrice){
-
-    }
-
-    public void billAuction(String user, long auctionID, double price){
-
-    }
-
-    public Bill getBill(String user){
-        return null;
-    }
-
-     private static class Bill {
-
-        public Bill() {
-        }
-    }
-
-    private static class PriceSteps {
-
-        public PriceSteps() {
-        }
-    }
-
+   public BillingServerSecure login(String username, String password){
+       return null;
+   }
 
 }
